38fb9d861191e05cc6d1af66b1b31d84cadd0a8e
Ana Damnjanovic <ana.damnjanovic@hotmail.com>
2025-05-09 23:24:22 -0400

    made InvertControlsEffect asset and added string labels to levels

--- TOjam2025/Assets/Scripts/Level.cs
+++ TOjam2025/Assets/Scripts/Level.cs
@@ -8,12 +8,18 @@ public class Level : ScriptableObject
     private int m_levelNum;
 
     [SerializeField]
+    private string m_levelText;
+
+    [SerializeField]
     private Effect m_levelEffect;
 
     [SerializeField]
     private List<GameObject> m_foodPrefabs;
 
     public int GetLevelNum => m_levelNum;
+
+    public string GetLevelText => m_levelText;
+
     public List<GameObject> GetFoodPrefabs => m_foodPrefabs;
 
     public Effect GetLevelEffect => m_levelEffect;
