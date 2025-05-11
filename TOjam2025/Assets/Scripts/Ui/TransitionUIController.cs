e87ca067bc3002fad2e15d15502096f892aa93f7
CarinaChimera <cachoy333@gmail.com>
2025-05-11 14:32:12 -0400

    transition panel 2

--- TOjam2025/Assets/Scripts/Ui/TransitionUIController.cs
+++ TOjam2025/Assets/Scripts/Ui/TransitionUIController.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using TMPro; 
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class TransitionUIController : MonoBehaviour
 {
@@ -9,12 +10,14 @@ public class TransitionUIController : MonoBehaviour
 
     [SerializeField]
     private Canvas m_canvas;
- 
+
+    [SerializeField] private GameObject secondPanel;
+
     [SerializeField]
     private Animator m_animator;
 
     [SerializeField]
-    private float m_displayTime = 2.5f;
+    private float m_displayTime = 5f;
 
     private float m_displayTimeElapsed = 0f;
 
@@ -22,10 +25,13 @@ public class TransitionUIController : MonoBehaviour
 
     public event System.Action TransitionFinished = delegate { };
 
+    
 
     public void ShowUi(int level, int maxLevel)
     {
-        int remainingLevels = Mathf.Max(0, maxLevel - level);
+        secondPanel.gameObject.SetActive(false);
+
+        int remainingLevels = Mathf.Max(0, 8 - level);
 
         nextLevelText.text = level.ToString();
         remainingLevelsText.text = remainingLevels.ToString();
@@ -36,13 +42,24 @@ public class TransitionUIController : MonoBehaviour
         m_displayTimeElapsed = 0f;
         m_animator.Play("Transition Anim");
         m_animationStarted = true;
+
+        StartCoroutine(ShowAndHide());
     }
 
-    public void HideUi()
+
+public IEnumerator ShowAndHide()
+    {
+        secondPanel.gameObject.SetActive(false);
+        yield return new WaitForSeconds(2);
+        secondPanel.gameObject.SetActive(true);
+    }
+
+public void HideUi()
     {
         m_canvas.gameObject.SetActive(false);
         m_canvas.enabled = false;
         m_animationStarted = false;
+        secondPanel.gameObject.SetActive(false);
     }
 
     private void Update()
